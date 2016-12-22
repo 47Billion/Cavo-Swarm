8c23d99d783001a2b66e1ced72887368edf2e824
Satish Verma <satishverma@gmail.com>
2016-12-22 09:49:47 +0000

    Start.sh update

--- manager-api/start.sh
+++ manager-api/start.sh
@@ -9,15 +9,15 @@ cd /var/www/api
 git clone https://$2@github.com/47Billion/Cavo-Swarm.git .
 git checkout $1
 git pull
-cd /var/www/api/manager-api/manager-api/
+cd /var/www/api/manager-api/manager-api
 npm install
 
 echo "pm2 start manager-api.js"
 pm2 start manager-api.js -i 1
 
 # Download zip file
-cd /usr/share/nginx/html/
-git clone https://github.com/softrobin5/zip-files.git .
+#cd /usr/share/nginx/html/
+#git clone https://github.com/softrobin5/zip-files.git .
 
 # cron
 
